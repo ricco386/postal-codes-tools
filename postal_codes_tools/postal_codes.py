38a4171483109391ccbc93aaf2c70b6ae9659667
Hnilica Jakub <jakub.hnilica@nbs.sk>
2021-09-08 12:36:21 +0000

    :sparkles: Rs 781

--- postal_codes_tools/postal_codes.py
+++ postal_codes_tools/postal_codes.py
@@ -1,7 +1,31 @@
+import re
 
-def verify_postal_code_format(postal_code: str, postal_code_country: str, allow_unknown_type: bool = True) -> bool:
+from postal_codes_tools.formats_constants import COUNTRIES_WITHOUT_POSTAL_CODE_SYSTEM
+from postal_codes_tools.formats_constants import POSTAL_CODES_REGEX
+
+
+def country_without_postal_code(country_iso_code) -> bool:
+    if country_iso_code in COUNTRIES_WITHOUT_POSTAL_CODE_SYSTEM:
+        return True
+    return False
+
+
+def has_postal_code(country_iso_code) -> bool:
+    if country_iso_code in POSTAL_CODES_REGEX:
+        return True
+    return False
+
+
+def verify_postal_code_format(postal_code: str, country_iso_code: str) -> bool:
     """
     Validate postal_code, based on its country
+
+    :param postal_code: String containing given postal code
+    :param country_iso_code: String with ISO 3166-1 alpha-2 code which is two letter country code (e.g. US, DE, SK)
+
+    :return:
+        function returns True/False according to presence of given combination of postal code and country
+        within POSTAL_CODES_REGEX dictionary
     """
-    # TODO: do the hard work ;)
-    return True
+
+    return bool(re.match(POSTAL_CODES_REGEX[country_iso_code], postal_code))
--- postal_codes_tools/tests/test_formats.py
+++ postal_codes_tools/tests/test_formats.py
@@ -1,6 +1,47 @@
+import pytest
+
 from postal_codes_tools.postal_codes import verify_postal_code_format
+from postal_codes_tools.postal_codes import country_without_postal_code
+from postal_codes_tools.postal_codes import has_postal_code
 
 
 def test_verify_postal_code_formats():
     # Samples from European Central Bank spreadsheet
-    assert verify_postal_code_format(postal_code='123', postal_code_country='DE') is True
+    assert verify_postal_code_format(postal_code="12345", country_iso_code="DE") is True
+    assert verify_postal_code_format(postal_code="123", country_iso_code="DE") is False
+    assert verify_postal_code_format(postal_code="AD123", country_iso_code="AD") is True
+    assert verify_postal_code_format(postal_code="AD1234", country_iso_code="AD") is False
+    assert verify_postal_code_format(postal_code="AD12", country_iso_code="AD") is False
+    assert verify_postal_code_format(postal_code="12345", country_iso_code="AD") is False
+
+
+def test_check_country_which_has_postal_code():
+    assert verify_postal_code_format(postal_code="TKCA 1ZZ", country_iso_code="TC") is True
+    assert verify_postal_code_format(postal_code="TKCA1ZZ", country_iso_code="TC") is False
+    assert verify_postal_code_format(postal_code="1234", country_iso_code="TC") is False
+    assert verify_postal_code_format(postal_code="", country_iso_code="TC") is False
+
+    assert country_without_postal_code("TC") is False
+    assert has_postal_code("TC") is True
+
+
+def test_verify_postal_code_for_countries_without_postal_code():
+    with pytest.raises(KeyError):
+        verify_postal_code_format(postal_code="", country_iso_code="AO")
+    with pytest.raises(KeyError):
+        assert verify_postal_code_format(postal_code="ASFAFS", country_iso_code="AO")
+
+    assert country_without_postal_code("AO") is True
+    assert country_without_postal_code("DE") is False
+
+    assert has_postal_code("AO") is False
+
+
+def test_verify_postal_code_for_unknown_countries():
+    with pytest.raises(KeyError):
+        assert verify_postal_code_format(postal_code="12345", country_iso_code="KK")
+    with pytest.raises(KeyError):
+        assert verify_postal_code_format(postal_code="", country_iso_code="KK")
+
+    assert country_without_postal_code("KK") is False
+    assert has_postal_code("KK") is False
